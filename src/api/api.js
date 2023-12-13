d46d6cb468a47325961108aa680e6ca5804204ad
dimaspng <d.sagachko@gmail.com>
2023-12-13 14:31:07 +0200

    started creating cart of product, added slider

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,15 @@
 export default async function getData() {
-  const request = await fetch("http://127.0.0.1:8000/api/data");
-  const data = await request.json();
-  const products = data.data.products;
-  const categories = data.data.categories;
-  return [categories, products];
+  try {
+    const request = await fetch("http://127.0.0.1:8000/api/data");
+    if (!request.ok) {
+      throw new Error(`HTTP error! Status: ${request.status}`);
+    }
+    const data = await request.json();
+    const products = data.data.products;
+    const categories = data.data.categories;
+    return [categories, products];
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+    return [null, null];
+  }
 }
