af92c86cfa64c7fd592d1286c85e7b9ef292a3d1
dimaspng <d.sagachko@gmail.com>
2023-12-25 12:38:57 +0200

    separated components and configured review ratings

---
+++ public/js/constants.js
@@ -0,0 +1,44 @@
+let isClickedStar1 = false;
+export function getIsClickedStar1() {
+  return isClickedStar1;
+}
+
+export function setIsClickedStar1(value) {
+  isClickedStar1 = value;
+}
+
+let isClickedStar2 = false;
+export function getIsClickedStar2() {
+  return isClickedStar2;
+}
+
+export function setIsClickedStar2(value) {
+  isClickedStar2 = value;
+}
+
+let isClickedStar3 = false;
+export function getIsClickedStar3() {
+  return isClickedStar3;
+}
+
+export function setIsClickedStar3(value) {
+  isClickedStar3 = value;
+}
+
+let isClickedStar4 = false;
+export function getIsClickedStar4() {
+  return isClickedStar4;
+}
+
+export function setIsClickedStar4(value) {
+  isClickedStar4 = value;
+}
+
+let isClickedStar5 = false;
+export function getIsClickedStar5() {
+  return isClickedStar5;
+}
+
+export function setIsClickedStar5(value) {
+  isClickedStar5 = value;
+}
